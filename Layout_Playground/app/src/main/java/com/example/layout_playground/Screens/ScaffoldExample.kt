0a74f7ff308937da00c7ddc1a31424333c0c1781
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-11-10 06:54:48 +0530

    Scaffold - TopAppBar | Done

--- Layout_Playground/app/src/main/java/com/example/layout_playground/Screens/ScaffoldExample.kt
+++ Layout_Playground/app/src/main/java/com/example/layout_playground/Screens/ScaffoldExample.kt
@@ -7,13 +7,17 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
+import com.example.layout_playground.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -24,9 +28,14 @@ fun MyScaffoldExample() {
     }
     Scaffold(
         topBar = {
-            TopAppBar(title = {
-                Text(text = "Top App bar")
-            })
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorResource(id = R.color.purple_200),
+                    titleContentColor = colorResource(id = R.color.black)
+                ),
+                title = {
+                    Text(text = "Top App bar")
+                })
         }
     ) { innerPadding ->
         Column(
