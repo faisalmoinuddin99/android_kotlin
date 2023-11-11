b2d1d8cad3de13d09c527b242613312ba35ad9a4
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-11-11 05:36:26 +0530

    NavigationDrawerItem | completed without applying padding

--- Layout_Playground/app/src/main/java/com/example/layout_playground/Screens/ScaffoldExample.kt
+++ Layout_Playground/app/src/main/java/com/example/layout_playground/Screens/ScaffoldExample.kt
@@ -14,20 +14,27 @@ import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -36,13 +43,14 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.layout_playground.R
 import com.example.layout_playground.model.NavigationItems
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyScaffoldExample() {
 
     // Present inside Navigation items
-    val item = listOf(
+    val items = listOf(
         NavigationItems(
             title = "All",
             selectedIcon = Icons.Filled.Home,
@@ -61,63 +69,118 @@ fun MyScaffoldExample() {
         ),
     )
 
+    // first create a drawer state hook - to open or close the navigation menu
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+
+    // define scope coroutine
+    val scope = rememberCoroutineScope()
+    var selectedItemIndex by rememberSaveable {
+        mutableStateOf(0)
+    }
+
     var presses by remember {
         mutableIntStateOf(0)
     }
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = colorResource(id = R.color.purple_200),
-                    titleContentColor = colorResource(id = R.color.black)
-                ),
-                title = {
-                    Text(text = "Top App bar")
-                },
-                navigationIcon = {
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu Icon")
-                    }
+    ModalNavigationDrawer(
+        drawerContent = {
+            ModalDrawerSheet {
+                items.forEachIndexed { index, item ->
+                    NavigationDrawerItem(
+                        label = {
+                            Text(text = item.title)
+                        },
+                        selected = index == selectedItemIndex,
+                        onClick = {
+                            selectedItemIndex = index
+                            scope.launch {
+                                drawerState.close()
+                            }
+                        },
+                        icon = {
+                            Icon(
+                                imageVector = if (index == selectedItemIndex) {
+                                    item.selectedIcon
+                                } else item.unselectedIcon,
+                                contentDescription = item.title
+                            )
+                        },
+                        badge = {
+                            // display only not null badge counts
+                            item.badgeCount?.let {
+                                item.badgeCount = presses
+                                Text(text = item.badgeCount.toString())
+                            }
+                        }
+                    )
                 }
-            )
+            }
         },
-        bottomBar = {
-            BottomAppBar(
-                containerColor = colorResource(id = R.color.purple_500),
-                contentColor = colorResource(id = R.color.white)
+        drawerState = drawerState
+    )
+    /* Below Piece of code will display the content outside the navigation drawer hence
+    its good to put Scaffold or any prefer layout
+     */
+    {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = colorResource(id = R.color.purple_200),
+                        titleContentColor = colorResource(id = R.color.black)
+                    ),
+                    title = {
+                        Text(text = "Top App bar")
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            scope.launch {
+                                drawerState.open()
+                            }
+                        }) {
+                            Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu Icon")
+                        }
+                    }
+                )
+            },
+            bottomBar = {
+                BottomAppBar(
+                    containerColor = colorResource(id = R.color.purple_500),
+                    contentColor = colorResource(id = R.color.white)
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        text = "Bottom app bar",
+                    )
+                }
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = { presses++ }) {
+                    Icon(Icons.Default.Add, contentDescription = "Add")
+
+                }
+            },
+
+            ) { innerPadding ->
+            Column(
+                modifier = Modifier.padding(innerPadding),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
                 Text(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    textAlign = TextAlign.Center,
-                    text = "Bottom app bar",
-                )
-            }
-        },
-        floatingActionButton = {
-            FloatingActionButton(onClick = { presses++ }) {
-                Icon(Icons.Default.Add, contentDescription = "Add")
-
-            }
-        },
-
-        ) { innerPadding ->
-        Column(
-            modifier = Modifier.padding(innerPadding),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            Text(
-                text =
-                """
+                    text =
+                    """
                     This is an example of a scaffold. It uses the Scaffold composable's parameters to create a screen with a simple top app bar, bottom app bar, and floating action button.
 
                     It also contains some basic inner content, such as this text.
 
                     You have pressed the floating action button $presses times.
                 """.trimIndent(),
-                modifier = Modifier.padding(8.dp)
-            )
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
         }
+
     }
 
 
--- Layout_Playground/app/src/main/java/com/example/layout_playground/model/NavigationItems.kt
+++ Layout_Playground/app/src/main/java/com/example/layout_playground/model/NavigationItems.kt
@@ -6,6 +6,6 @@ data class NavigationItems(
     val title: String,
     val selectedIcon: ImageVector,
     val unselectedIcon: ImageVector,
-    val badgeCount: Int? = null
+    var badgeCount: Int? = null
 
 )
