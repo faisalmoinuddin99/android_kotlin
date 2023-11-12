fe77d967b945143deb00fc2118f0846577e2e061
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-11-12 06:40:35 +0530

    Vertical Scrolling Example | Part 2 | Done with adding Vertical Scrolling Feature

--- Layout_Playground/app/src/main/java/com/example/layout_playground/Screens/ScrollingScreen.kt
+++ Layout_Playground/app/src/main/java/com/example/layout_playground/Screens/ScrollingScreen.kt
@@ -3,9 +3,16 @@ package com.example.layout_playground.Screens
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
@@ -16,15 +23,23 @@ import com.example.layout_playground.R
 
 @Composable
 fun MyScrollingScreen(modifier: Modifier = Modifier) {
-    Column {
+    Column(
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
+            .padding(10.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+
+        ) {
         BookImage(
             imageResId = R.drawable.book_cover_1,
             contentDescriptionId = R.string.first_book_1
         )
+
         BookImage(
             imageResId = R.drawable.book_cover_2,
             contentDescriptionId = R.string.first_book_2
         )
+
         BookImage(
             imageResId = R.drawable.book_cover_3,
             contentDescriptionId = R.string.first_book_3
