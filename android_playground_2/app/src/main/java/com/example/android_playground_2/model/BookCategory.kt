fdc54bb521f8b60e6455e11350248e76b98c4c5c
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-11-14 18:32:51 +0530

    JetNotes Project setup completed

--- android_playground_2/app/src/main/java/com/example/android_playground_2/model/BookCategory.kt
+++
@@ -1,8 +0,0 @@
-package com.example.android_playground_2.model
-
-import androidx.annotation.StringRes
-
-data class BookCategory(
-    @StringRes val categoryResourceId : Int,
-    val bookImageRes : List<Int>
-)
