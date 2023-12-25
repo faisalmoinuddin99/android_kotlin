5dcf7280a98d2b406615319de503bc64f4f2dbc5
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-12-25 15:24:17 +0530

    Adding a background to the Note

--- app/src/main/java/com/example/jetnotes/ui/components/Note.kt
+++ app/src/main/java/com/example/jetnotes/ui/components/Note.kt
@@ -5,12 +5,18 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,16 +25,22 @@ import com.example.jetnotes.ui.theme.green
 
 @Composable
 fun Note() {
-
+    val backgroundShape: Shape = RoundedCornerShape(4.dp)
     Row(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .padding(8.dp)
+            .shadow(1.dp, backgroundShape)
+            .fillMaxWidth()
+            .heightIn(min = 64.dp)
+            .background(Color.White, backgroundShape)
+
     ) {
-       NoteColor(color = green, size = 40.dp, border = 1.dp, padding = 4.dp)
+        NoteColor(color = green, size = 40.dp, border = 1.dp, padding = 4.dp)
         Column(
             modifier = Modifier.weight(1f)
         ) {
-            Text(text = "Title", maxLines = 1)
-            Text(text = "Content", maxLines = 1)
+            Text(text = "Title", maxLines = 1, color = Color.Black)
+            Text(text = "Content", maxLines = 1, color = Color.Black)
         }
         Checkbox(
             checked = false, onCheckedChange = {},
